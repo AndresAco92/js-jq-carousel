ed9b486d8736c760e198b25a977f45ab3689de10
AndresAco92 <andres.antonio.aco92@gmail.com>
2021-04-20 23:31:25 +0200

    Essercizio finitoù

--- assets/script.js
+++ assets/script.js
@@ -4,30 +4,38 @@ $(function(){
 
   function dopo(){
     var on_active = $('img.active');
-    var on_active_circle = $('i.active_circle');
+    var on_active_circle = $('li.active');
 
     on_active.removeClass('active');
-    on_active_circle.removeClass('active_circle');
+    on_active_circle.removeClass('active');
 
     if(on_active.hasClass('last')){
       $('img.first').addClass('active');
-      $('i.first').addClass('active_circle');
+      $('li.first').addClass('active');
 
     }else{
       on_active.next().addClass('active');
-      on_active_circle.next().addClass('active_circle');
+      on_active_circle.next().addClass('active');
     }
   }
-
+     
+  
   $('.before > .fa-chevron-left').click(prima);
 
   function prima(){
     var on_active = $('img.active');
+    var on_active_circle = $('li.active');
+
     on_active.removeClass('active');
+    on_active_circle.removeClass('active');
+
     if(on_active.hasClass('first')){
       $('img.last').addClass('active');
+      $('li.last').addClass('active');
+
     }else{
-      on_active.prev('img').addClass('active');
+      on_active.prev().addClass('active');
+      on_active_circle.prev().addClass('active');
     }
 
   }
